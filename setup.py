a369c6f0d18fe8325923691af8a9476fa83b6e15
Amr Hassan <amr.hassan@gmail.com>
2008-09-09 19:22:13 +0000

    * added a setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pylast',
+      version='0.2b8',
+      author='Amr Hassan',
+      author_email='amr.hassan@gmail.com',
+      url='http://code.google.com/p/pylast/',
+      py_modules= ("pylast",)
+)
