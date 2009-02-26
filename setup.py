52aabb750a6648714f998e6afae22901d6cfd31d
Amr Hassan <amr.hassan@gmail.com>
2009-02-26 05:09:47 +0000

    * better setup script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import pylast
 setup(name = pylast.__name__,
       version = pylast.__version__,
       author = pylast.__author__,
-      long_description = pylast.__doc__,
+	  description = pylast.__doc__,
       author_email = pylast.__email__,
       url='http://code.google.com/p/pylast/',
       py_modules= ("pylast",)
