b4ea08a378bd12c823b6e68f4b72f3a6b327f8e1
hugovk <hugovk@users.noreply.github.com>
2014-07-25 17:01:14 +0300

    Change URL, add classifiers and keywords

--- setup.py
+++ setup.py
@@ -29,7 +29,20 @@ setup(
     author="Amr Hassan <amr.hassan@gmail.com>",
     description="A Python interface to Last.fm (and other API compatible social networks)",
     author_email="amr.hassan@gmail.com",
-    url="https://github.com/hugovk/pylast",
+    url="https://github.com/pylast/pylast",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        ],
+    keywords=["Last.fm", "music", "scrobble", "scrobbling"],
     py_modules=("pylast",),
     license="Apache2"
     )
