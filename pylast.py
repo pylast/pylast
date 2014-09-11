56d1f2791f78045e111b542032df08428dfd1470
Hugo <hugovk@users.noreply.github.com>
2014-09-11 15:29:25 +0300

    Fix chart tests that failed due to date ranges

--- pylast.py
+++ pylast.py
@@ -4,6 +4,7 @@
 #     A Python interface to Last.fm (and other API compatible social networks)
 #
 # Copyright 2008-2010 Amr Hassan
+# Copyright 2013-2014 hugovk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,8 +21,8 @@
 # http://code.google.com/p/pylast/
 
 __version__ = '1.0.0'
-__author__ = 'Amr Hassan'
-__copyright__ = "Copyright (C) 2008-2010  Amr Hassan"
+__author__ = 'Amr Hassan, hugovk'
+__copyright__ = "Copyright (C) 2008-2010  Amr Hassan, 2013-2014 hugovk"
 __license__ = "apache2"
 __email__ = 'amr.hassan@gmail.com'
 
