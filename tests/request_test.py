c2b03afe559c4af1c77d40c02bfc7d7d01e039c9
Ivan Malison <IvanMalison@gmail.com>
2015-01-05 15:55:45 -0800

    Don't encode strings as 'utf-8' twice in _get_cache_key. Closes #109.

---
+++ tests/request_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import mock
+import pytest
+
+import pylast
+
+
+def mock_network():
+    return mock.Mock(
+        _get_ws_auth=mock.Mock(return_value=("", "", ""))
+    )
+
+@pytest.mark.parametrize('unicode_artist', [u'\xe9lafdasfdsafdsa', u'éééééééé'])
+def test_get_cache_key(unicode_artist):
+    request = pylast._Request(mock_network(), 'some_method',
+                              params={'artist': unicode_artist})
+    request._get_cache_key()
