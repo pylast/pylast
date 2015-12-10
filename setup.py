0938c596f82178486b8e0b5fae2e6043d49b174b
hugovk <hugovk@users.noreply.github.com>
2015-12-10 21:08:46 +0200

    Release 1.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pylast",
-    version="1.5.0",
+    version="1.5.1",
     author="Amr Hassan <amr.hassan@gmail.com>",
     install_requires=['six'],
     tests_require=['mock', 'pytest', 'coverage', 'pep8', 'pyyaml', 'pyflakes'],
