b410ae8b8dea9bd03519c8c260271be2f11d410e
Hugo <hugovk@users.noreply.github.com>
2017-01-02 21:07:21 +0100

    Add Python 3.5 and 3.6 (#188)

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     keywords=["Last.fm", "music", "scrobble", "scrobbling"],
     packages=find_packages(exclude=('tests*',)),
