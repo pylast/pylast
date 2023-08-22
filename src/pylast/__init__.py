3ece8e83a1b2510c5a48bec549c43ad1b771e632
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-22 14:10:40 +0300

    Fix E721 do not compare types, for exact checks use 'is' / 'is not', for instance checks use 'isinstance()'

--- src/pylast/__init__.py
+++ src/pylast/__init__.py
@@ -1505,7 +1505,7 @@ class _Opus(_Taggable):
         return f"{self.get_artist().get_name()} - {self.get_title()}"
 
     def __eq__(self, other):
-        if type(self) != type(other):
+        if not isinstance(self, other):
             return False
         a = self.get_title().lower()
         b = other.get_title().lower()
